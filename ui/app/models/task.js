924f2e69d49eb231fe6ee961a0407ff1e38064ce
Spencer Schack <scs1818@gmail.com>
2017-02-05 15:07:29 -0800

    tasks table

--- ui/app/models/task.js
+++ ui/app/models/task.js
@@ -1,7 +1,23 @@
 import DS from 'ember-data';
+import service from 'ember-service/inject';
+import computed from 'ember-computed-decorators';
 
 export default DS.Model.extend({
+
+  moment: service(),
+
   name: DS.attr('string'),
   start: DS.attr('date'),
-  finish: DS.attr('date')
+  finish: DS.attr('date'),
+
+  @computed('start', 'finish')
+  duration(start, finish) {
+    start = this.get('moment').moment(start);
+    if(finish) {
+      finish = this.get('moment').moment(finish);
+      return finish.diff(start);
+    }
+    return start.toNow();
+  }
+
 });
